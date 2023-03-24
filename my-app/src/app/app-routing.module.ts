9b7799e4f1147a66040c38d32a8e7a2db3521c6a
Yu Chong <evan100225@gmail.com>
2023-03-24 13:41:19 +0800

    feat: router setting

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./view/index/index.module').then((m) => m.IndexModule),
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
